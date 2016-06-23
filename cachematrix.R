8246c2faac3f73b1a50657ce98341a3ece2796d4
Jenifer E. Allsworth, PhD <allsworthj@umkc.edu>
2016-06-23 16:20:26 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming  - Assignment 2
+## Write 2 functions, makeCacheMatrix and cacheSolve
+## makeCacheMatrix creates an object that can cache its inverse
+## cacheSolve computes the inverse of the object created by makeCacheMatrix
 
-## Write a short comment describing this function
 
+## Function 1: makeCachematrix
+##             assign, get, define inverse and retrieve inverse
 makeCacheMatrix <- function(x = matrix()) {
+     i <- NULL
+     set <- function(matrix) {
+          m <<- matrix
+          i <<- NULL
+     }
 
+     get <- function() {m}
+     
+     defineinverse <- function(inverse) {
+          i <<- inverse
+     }
+     retrieveinverse <- function() {i}
+     
 }
 
 
-## Write a short comment describing this function
+## Function 2: cacheSolve
+## compute inverse of matrix created by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       m <-x$retrieveinverse()
+       m <- solve(data) %*% data
+       x$defineinverse(m)
+       m
 }
